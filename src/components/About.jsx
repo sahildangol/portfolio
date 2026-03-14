36484ecea93782732c45d361daad1186a05d80cf
sahildangol <sahildangol2004@gmail.com>
2026-03-14 21:29:30 +0545

    Minor Fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,6 @@ const About = () => {
           </motion.div>
         ))}
       </motion.div>
-
     </>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from "./canvas";
+import {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
